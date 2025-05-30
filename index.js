cee75614fa9f2fc9668727a6b72bcebb63b18270
Yuta Hiroto <git@hiroppy.me>
2025-05-31 05:55:06 +0900

    refactor: clean up

--- index.js
+++
@@ -1,7 +0,0 @@
-import { writeFile } from "node:fs/promises";
-import { getSponsors } from "./sponsors.js";
-import { generateREADME } from "./readme.js";
-
-const sponsors = await getSponsors();
-
-await writeFile("./README.md", generateREADME(sponsors));
