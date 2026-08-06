bdc0132899039fb9446f0127ab9bd950e15dfc61
GitHub Action <action@github.com>
2026-08-06 10:55:34 +0000

    feat: update generated files from data changes
    - Updated generated files based on changes in data/ directory
    - Build triggered by: Add Japan Toreca Center advisor role

--- generated/types/talks.d.ts
+++ generated/types/talks.d.ts
@@ -12,8 +12,8 @@ export interface Talk {
   description: string;
   image: string;
   name: string;
-  favicon: string;
   url: string;
+  favicon: string;
   publishedAt: string;
   links: LinkMeta[];
 }
